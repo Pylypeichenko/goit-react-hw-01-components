9aaf75fd998025c3a8de4e6de98708078479202e
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-10-09 13:52:05 +0300

    Update Statistics.jsx

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -22,7 +22,13 @@ const Statistics = ({ title, stats }) => {
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.arrayOf(PropTypes.object),
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
 };
 
 export default Statistics;
